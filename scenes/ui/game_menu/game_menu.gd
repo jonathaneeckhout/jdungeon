5112fcf58baab91a3d924a998ae985b24b0f8d45
Toprak2 <115584002+Toprak2@users.noreply.github.com>
2023-10-07 12:38:45 +0300

    Add a game menu that has a quit game option

--- scenes/ui/game_menu/game_menu.gd
+++ scenes/ui/game_menu/game_menu.gd
@@ -2,3 +2,19 @@ extends Control
 
 @onready var quit_button := $Panel/VBoxContainer/MarginContainer/QuitButton
 
+func _ready():
+	quit_button.pressed.connect(_on_quit_button_pressed)
+
+func _input(event):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		if self.is_visible_in_tree():
+			self.hide()
+		else :
+			self.show()
+
+func  _on_quit_button_pressed():
+	var quit_game_callable :Callable = Callable(self,"quit_game")
+	JUI.confirmationbox("Are you sure you want to quit the game?",self,"Quit Game?",quit_game_callable)
+
+func quit_game():
+	get_tree().quit()
