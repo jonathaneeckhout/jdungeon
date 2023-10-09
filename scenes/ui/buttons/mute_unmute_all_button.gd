9f5474c917b5787b7cbb35e7418693c20b683ad4
Toprak2 <muhammedalitoprak1@gmail.com>
2023-10-09 16:08:04 +0300

    Add a new ui button that mutes/unmutes all sound

---
+++ scenes/ui/buttons/mute_unmute_all_button.gd
@@ -0,0 +1,22 @@
+extends TextureButton
+
+@export var sound_on:Texture2D
+@export var sound_off:Texture2D
+
+@onready var master_sound = AudioServer.get_bus_index("Master")
+func _ready():
+	self.pressed.connect(_on_button_pressed)
+	
+	if not AudioServer.is_bus_mute(master_sound):
+		self.set_texture_normal(sound_on)
+	else :
+		self.set_texture_normal(sound_off)
+
+
+func _on_button_pressed():
+	if not AudioServer.is_bus_mute(master_sound):
+		AudioServer.set_bus_mute(master_sound,true)
+		self.set_texture_normal(sound_off)
+	else :
+		AudioServer.set_bus_mute(master_sound,false)
+		self.set_texture_normal(sound_on)
