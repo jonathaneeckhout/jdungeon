bb5960faa3cc38d536f27f6e6407c51107f56d06
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2023-10-15 21:36:28 +0200

    make the deadpopup menu more efficient

--- scenes/player/deathpopup/DeathPopup.gd
+++ scenes/player/deathpopup/DeathPopup.gd
@@ -1,9 +1,10 @@
 extends Control
 
-signal respawn_player
 var respawn_time: float = J.PLAYER_RESPAWN_TIME
-var counter = respawn_time
+
 var timer: Timer
+var countdown_timer: Timer
+
 @onready var count_down_label: Label = $Panel/CountdownLabel
 
 
@@ -11,28 +12,33 @@ func _ready():
 	timer = Timer.new()
 	timer.name = "Timer"
 	timer.autostart = false
+	timer.one_shot = true
 	timer.timeout.connect(_on_timer_timeout)
 	add_child(timer)
+
+	countdown_timer = Timer.new()
+	countdown_timer.name = "CountDownTimer"
+	countdown_timer.autostart = false
+	countdown_timer.wait_time = 1.0
+	countdown_timer.timeout.connect(_on_countdown_timer_timeout)
+	add_child(countdown_timer)
 
 
 func show_popup():
 	self.show()
 	timer.start(respawn_time)
+	countdown_timer.start(0.1)
+	update_counter()
+
+
+func update_counter():
+	count_down_label.text = ("%d:%02d" % [floor(timer.time_left / 60), int(timer.time_left) % 60])
 
 
 func _on_timer_timeout():
-	emit_signal("respawn_player")
+	countdown_timer.stop()
 	self.hide()
 
 
-func _process(_delta):
-	if !self.visible:
-		return
-
-	if counter > 0:
-		counter -= 1
-		count_down_label.text = (
-			"%d:%02d" % [floor(timer.time_left / 60), int(timer.time_left) % 60]
-		)
-	else:
-		counter = respawn_time
+func _on_countdown_timer_timeout():
+	update_counter()
