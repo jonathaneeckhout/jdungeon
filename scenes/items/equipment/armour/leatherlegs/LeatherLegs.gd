a9549d4204c61177c1c382701eb1a1713dd8fe64
NancokPS2 <nancok@tuta.io>
2023-10-19 10:30:24 -0300

    Test stat increase for leather legs

--- scenes/items/equipment/armour/leatherlegs/LeatherLegs.gd
+++ scenes/items/equipment/armour/leatherlegs/LeatherLegs.gd
@@ -7,3 +7,11 @@ func _init():
 	item_class = "LeatherLegs"
 	itemType = ItemTypes.EQUIPMENT
 	equipment_slot = "Legs"
+	
+	var newBoost:=JStats.Boost.new()
+	newBoost.statKey = JStats.Keys.DEFENSE
+	newBoost.value = 10
+	newBoost.type = JStats.Boost.Types.ADDITIVE
+	newBoost.stackSource = uuid
+	
+	equipment_boosts.append(newBoost)
