0b4fb64ff3d6c9d1b4f68ae196fa884105216de8
NancokPS2 <nancok@tuta.io>
2023-10-20 19:32:20 -0300

    Small fix

--- scenes/player/statdisplay/StatDisplay.gd
+++ scenes/player/statdisplay/StatDisplay.gd
@@ -52,6 +52,9 @@ class SingleStat extends HSplitContainer:
 		labelStatValue.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 		add_child(labelStatValue)
 		
+		for stat in JStats.Keys.values():
+			_on_stat_changed(stat)
+		
 	func _on_stat_changed(statKey: String):
 		if NodePath(statKey) == propertyPath:
 			labelStatValue.text = str( statsObject.stat_get(statName) )
