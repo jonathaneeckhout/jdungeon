557f5c69021c6670bec445555d4a20f95f2259e0
NancokPS2 <nancok@tuta.io>
2023-10-20 19:09:13 -0300

    Sample stat boost definition for weapon

--- scenes/items/equipment/weapons/sword/Sword.gd
+++ scenes/items/equipment/weapons/sword/Sword.gd
@@ -7,3 +7,12 @@ func _init():
 	item_class = "Sword"
 	itemType = ItemTypes.EQUIPMENT
 	equipment_slot = "RightHand"
+	
+	
+	var newBoost:=JStats.Boost.new()
+	newBoost.statKey = JStats.Keys.ATTACK_DAMAGE
+	newBoost.value = 10
+	newBoost.type = JStats.Boost.Types.ADDITIVE
+	newBoost.stackSource = uuid
+	
+	equipment_boosts.append(newBoost)
