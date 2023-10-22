2dc96833524302fefe7a029538dc10b7518b6d47
NancokPS2 <nancok@tuta.io>
2023-10-22 14:48:58 -0300

    Removed redundant variable

--- scenes/player/statdisplay/StatDisplay.gd
+++ scenes/player/statdisplay/StatDisplay.gd
@@ -22,42 +22,41 @@ func renew_displays():
 		add_single_stat(statName, statName)
 
 func add_single_stat(statName:String, statKeyUsed:NodePath):
-	var newStat := SingleStat.new(user.stats, statName, statKeyUsed)
+	var newStat := SingleStat.new(user.stats, statKeyUsed)
 
 	targetContainer.add_child(newStat)
 	
 class SingleStat extends HSplitContainer:
 	
 	var statsObject: JStats
-	var statKeyUsed: NodePath
-	var statName: String
+	var statKeyUsed: String
 	
 	var labelStatName := Label.new()
 	var labelStatValue := Label.new()
 	
-	func _init(i_statsObject:Node, i_statName:String, i_statKeyUsed:NodePath) -> void:
+	func _init(i_statsObject:Node, i_statKeyUsed:String) -> void:
 		statsObject = i_statsObject
 		statKeyUsed = i_statKeyUsed
-		statName = i_statName
 		
 		statsObject.stat_changed.connect( _on_stat_changed )
 		dragger_visibility = SplitContainer.DRAGGER_HIDDEN
 		
 	func _ready() -> void:
-		set_name(statName)
+		set_name( tr(statKeyUsed) )
 		
-		labelStatName.text = tr(statName)
+		labelStatName.text = tr(statKeyUsed)
 		labelStatName.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 		add_child(labelStatName)
 		
-		labelStatValue.text = str( statsObject.stat_get(statName) )
+		labelStatValue.text = str( statsObject.stat_get(statKeyUsed) )
 		labelStatValue.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 		add_child(labelStatValue)
+		
 		
 		for stat in JStats.Keys.values():
 			_on_stat_changed(stat)
 		
 	func _on_stat_changed(statKey: String):
-		if NodePath(statKey) == statKeyUsed:
-			labelStatValue.text = str( statsObject.stat_get(statKey) )
+		if statKey == statKeyUsed:
+			labelStatValue.text = str( statsObject.stat_get(statKeyUsed) )
 
