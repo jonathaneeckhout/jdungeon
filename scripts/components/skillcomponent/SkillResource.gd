f5342cf61c90f190cf1193ef778d87e94738ba99
NancokPS2 <nancok@tuta.io>
2023-11-06 12:06:52 -0300

    Skill component expansion

--- scripts/components/skillcomponent/SkillResource.gd
+++ scripts/components/skillcomponent/SkillResource.gd
@@ -1,21 +1,27 @@
 extends Resource
 class_name SkillComponentResource
 
+enum ControlType {TARGETABLE, INSTANT}
+
 @export var displayed_name: String = "Skidadle Skidoodle" #Failsafe name
+
 @export var energy_usage: int = 0
-@export var hitbox_shape: PackedVector2Array
-@export var hit_range: float
+
+@export var cooldown: float = 0
+
+## Use a single point to create a circle using the single point as radius, use more than 3 points to create a polygon
+@export var hitbox_shape: PackedVector2Array = [Vector2.LEFT*5, Vector2.DOWN*5, Vector2.RIGHT*5, Vector2.UP*5]
+
+#Skills cannot be used past this range
+@export var hit_range: float = 25
+
+@export var controlType: ControlType = ControlType.TARGETABLE
 
 
 func effect(targets: Array[Node]):
 	for target in targets:
 		_effect(target)
 
-func target_filter(targets: Array[Node])->bool:
-	for target in targets:
-		if not _target_filter(target):
-			return false
-	return true
 
 func _target_filter(target: Node)->bool:
 	return true
--- scripts/components/skillcomponent/skillcomponent.gd
+++ scripts/components/skillcomponent/skillcomponent.gd
@@ -1,17 +1,63 @@
 extends Node
 class_name SkillComponent
 
+signal skill_successful_usage(skill: SkillComponentResource)
+signal skill_failed_usage(skill: SkillComponentResource)
+signal skill_selected(skill: SkillComponentResource)
+
+const COLOR_HITBOX := Color.AQUA / 2
+const COLOR_HITBOX_UNUSABLE := Color.FIREBRICK / 2
+const COLOR_RANGE := Color.GREEN_YELLOW / 2
+
+
 @export var user: CharacterBody2D:
 	set(val):
 		user = val
 		directSpace = user.get_world_2d().direct_space_state
-@export var stats: StatsSynchronizerComponent
+		user.draw.connect(draw_on_user)
+		
+@export var stats_component: StatsSynchronizerComponent
+@export var input_component: InputSynchronizerComponent
 
 @export var skills: Array[SkillComponentResource]
-@export var skill_current: SkillComponentResource
+@export var skill_current: SkillComponentResource:
+	set(val):
+		skill_current = val
+		skill_selected.emit(skill_current)
 
+
+var cooldownDict: Dictionary
 var directSpace: PhysicsDirectSpaceState2D
 var shapeParameters := PhysicsShapeQueryParameters2D.new()
+
+func draw_on_user():
+	if not skill_current:
+		return
+	draw_range()
+	draw_hitbox()
+func draw_range():
+	user.draw_circle(Vector2.ZERO, skill_current.hit_range, COLOR_RANGE)
+func draw_hitbox(atPoint: Vector2 = input_component.get_global_mouse_position()):
+
+	var shape: Shape2D = get_collision_shape()
+	var colorUsed: Color
+	
+	#Color selection
+	if is_skill_usable(skill_current):
+		colorUsed = COLOR_HITBOX
+	else:
+		colorUsed = COLOR_HITBOX_UNUSABLE
+	
+	#Drawing
+	if shape is CircleShape2D:
+		user.draw_circle(atPoint, shape.radius, colorUsed)
+		
+	elif shape is ConvexPolygonShape2D:
+		var polygon: PackedVector2Array
+		for point in shape.points:
+			polygon.append(point + atPoint)
+			
+		user.draw_colored_polygon(polygon, colorUsed)
 
 func skill_select_by_index(index: int):
 	#Array supports negative values to select from the end of the Array
@@ -20,28 +66,68 @@ func skill_select_by_index(index: int):
 		return
 		
 	skill_current = skills[index]
-	pass
+
+func skill_deselect():
+	skill_current = null
 
 func use_at(globalPoint: Vector2):
-	var targets: Array[CollisionObject2D] = get_targets(globalPoint)
+	if not is_skill_usable(skill_current):
+		skill_failed_usage.emit(skill_current)
+		return
+	
+	if user.global_position.distance_to(globalPoint) > skill_current.hit_range:
+		skill_failed_usage.emit(skill_current)
+		return
+	
+	var targets: Array[Node] = get_targets(globalPoint)
+	handle_cooldown(skill_current, true)
 	skill_current.effect(targets)
+	skill_successful_usage.emit(skill_current)
 	
 	
-func get_targets(where: Vector2)->Array[CollisionObject2D]:
-	update_collision_parameters()
+func get_targets(where: Vector2)->Array[Node]:
+	shapeParameters.shape = get_collision_shape()
 	
 	var collisions: Array[Dictionary] = directSpace.intersect_shape(shapeParameters)
-	var targets: Array[CollisionObject2D] = []
+	var targets: Array[Node] = []
 	for coll in collisions:
 		targets.append(coll.get("collider"))
-	
+		
+	targets.filter(skill_current._target_filter)
 	return targets
-
-func get_user_facing()->Vector2:
-	return Vector2.RIGHT
-	pass
 	
-func update_collision_parameters():
-	var shape := ConvexPolygonShape2D.new()
-	shape.points = skill_current.hitbox_shape
-	shapeParameters.shape = shape
+func get_collision_shape()->Shape2D:
+	var shape: Shape2D
+	#Do not allow shapes with a size of 2
+	assert(skill_current.hitbox_shape.size() != 2)
+	
+	#If it is only 1 point, treat it as a circle
+	if skill_current.hitbox_shape.size() == 1:
+		shape = CircleShape2D.new()
+		shape.radius = skill_current.hitbox_shape[0]
+		
+	#Otherwise treat it as a polygon
+	else:
+		shape = ConvexPolygonShape2D.new()
+		shape.points = skill_current.hitbox_shape
+		
+	return shape
+
+func handle_cooldown(skill: SkillComponentResource, started: bool):
+	#Start the cooldown and set the skill as "cooling down"
+	if started:
+		cooldownDict[skill] = true
+		get_tree().create_timer(skill.cooldown).timeout.connect(handle_cooldown.bind(false))
+	#Remove the skill from the "cooling down" list
+	else:
+		cooldownDict.erase(skill)
+	
+func is_skill_usable(skill: SkillComponentResource)->bool:
+	if not stats_component.energy >= skill.energy_usage:
+		return false
+	
+	if cooldownDict.has(skill):
+		return false
+		
+	return true
+	
