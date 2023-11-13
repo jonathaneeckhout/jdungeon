3fdacd5ac80fefd6abc17827747935f16655e7cd
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2023-11-13 21:58:58 +0100

    move common code to single file

--- scripts/singletons/gameserver.gd
+++ scripts/singletons/gameserver.gd
@@ -14,6 +14,7 @@ var mode: MODE = MODE.CLIENT
 
 var users: Dictionary = {}
 
+var dtls_networking: DTLSNetworking
 var database: Database
 var message_handler: MessageHandler
 
@@ -28,6 +29,10 @@ var clock_sync_timer: Timer
 var world: World = null
 
 var client_player: Player = null
+
+
+func _ready():
+	dtls_networking = DTLSNetworking.new()
 
 
 func init_common() -> bool:
@@ -56,39 +61,21 @@ func server_init(port: int, max_clients: int, cert_path: String, key_path: Strin
 		GodotLogger.error("Failed to init gameserver's common part")
 		return false
 
-	database = Database.new()
-	database.name = "Database"
-	add_child(database)
+	database = dtls_networking.add_database(self)
+	if database == null:
+		GodotLogger.error("Failed add database")
+		return false
 
 	message_handler = MessageHandler.new()
 	message_handler.name = "MessageHandler"
 	add_child(message_handler)
 
-	if not database.init():
-		GodotLogger.error("Failed to init server's database")
+	var server: ENetMultiplayerPeer = dtls_networking.server_init(
+		port, max_clients, cert_path, key_path
+	)
+	if server == null:
+		GodotLogger.error("Failed create server")
 		return false
-
-	var server: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
-
-	var error = server.create_server(port, max_clients)
-	if error != OK:
-		GodotLogger.error("Failed to create server")
-		return false
-
-	if server.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
-		GodotLogger.error("Failed to start server")
-		return false
-
-	if not Global.env_no_tls:
-		var server_tls_options = server_get_tls_options(cert_path, key_path)
-		if server_tls_options == null:
-			GodotLogger.error("Failed to load tls options")
-			return false
-
-		error = server.host.dtls_server_setup(server_tls_options)
-		if error != OK:
-			GodotLogger.error("Failed to setup DTLS")
-			return false
 
 	multiplayer.multiplayer_peer = server
 
@@ -156,45 +143,6 @@ func client_connect(address: String, port: int) -> bool:
 	GodotLogger.info("Started client")
 
 	return true
-
-
-func server_get_tls_options(cert_path: String, key_path: String) -> TLSOptions:
-	if not FileAccess.file_exists(cert_path):
-		GodotLogger.error("Certificate=[%s] does not exist" % cert_path)
-		return null
-
-	if not FileAccess.file_exists(key_path):
-		GodotLogger.error("Key=[%s] does not exist" % key_path)
-		return null
-
-	var cert_file = FileAccess.open(cert_path, FileAccess.READ)
-	if cert_file == null:
-		GodotLogger.error("Failed to open server certificate %s" % cert_path)
-		return null
-
-	var key_file = FileAccess.open(key_path, FileAccess.READ)
-	if key_file == null:
-		GodotLogger.error("Failed to open server key %s" % key_path)
-		return null
-
-	var cert_string = cert_file.get_as_text()
-	var key_string = key_file.get_as_text()
-
-	var cert = X509Certificate.new()
-
-	var error = cert.load_from_string(cert_string)
-	if error != OK:
-		GodotLogger.error("Failed to load certificate")
-		return null
-
-	var key = CryptoKey.new()
-
-	error = key.load_from_string(key_string)
-	if error != OK:
-		GodotLogger.error("Failed to load key")
-		return null
-
-	return TLSOptions.server(key, cert)
 
 
 func is_server() -> bool:
