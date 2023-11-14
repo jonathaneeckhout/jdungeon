3c866d1c82d78f8b1f925da61f2b90f6bd75439e
NancokPS2 <nancok@tuta.io>
2023-11-14 13:00:38 -0300

    More formatting

--- scripts/components/skillcomponent/SkillResource.gd
+++ scripts/components/skillcomponent/SkillResource.gd
@@ -3,7 +3,7 @@ class_name SkillComponentResource
 
 @export var skill_class: String
 
-@export var displayed_name: String = "Skidadle Skidoodle" #Failsafe name
+@export var displayed_name: String = "Skidadle Skidoodle"  #Failsafe name
 
 @export var energy_usage: int = 0
 
@@ -19,7 +19,9 @@ class_name SkillComponentResource
 @export var damage: int = 0
 
 ## Use a single point to create a circle using the single point as radius, use more than 3 points to create a polygon
-@export var hitbox_shape: PackedVector2Array = [Vector2.LEFT*5, Vector2.DOWN*5, Vector2.RIGHT*5, Vector2.UP*5]
+@export var hitbox_shape: PackedVector2Array = [
+	Vector2.LEFT * 5, Vector2.DOWN * 5, Vector2.RIGHT * 5, Vector2.UP * 5
+]
 
 #This prevents the hitbox from detecting the user, does not prevent effects that directly reference it.
 @export var hitbox_hits_user: bool = false
@@ -30,19 +32,18 @@ class_name SkillComponentResource
 #Skills cannot be used past this range
 @export var hit_range: float = 100
 
+
 func effect(information: SkillComponent.UseInfo):
 	if damage > 0:
 		for stats in information.get_target_stats_all():
 			stats.hurt(information.user, damage)
-	
+
 	_effect(information)
 
 
-func _target_filter(_target: Node)->bool:
+func _target_filter(_target: Node) -> bool:
 	return true
 
-func _effect(_info: SkillComponent.UseInfo):
-	
-	pass
-	
 
+func _effect(_info: SkillComponent.UseInfo):
+	pass
