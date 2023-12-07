bcb50383a643e1522fa76c6e0653d567d0a8ffae
NancokPS2 <nancok@tuta.io>
2023-12-07 09:41:48 -0300

    Static typing for loops

--- scripts/components/charclasscomponent/characterclasscomponent.gd
+++ scripts/components/charclasscomponent/characterclasscomponent.gd
@@ -63,7 +63,8 @@ func remove_class(charclass: String):
 	if classes.is_empty():
 		GodotLogger.warn("Tried to remove a class but there are none in this component.")
 		return
-	
+		
+	# Clean the classes Array of any class matching this charclass
 	var index: int = 0
 	while index < classes.size():
 		if classes[index].class_registered == charclass:
@@ -82,7 +83,7 @@ func add_class(charclass: String, bypassLimit: bool = false):
 		GodotLogger.warn("Added a blacklisted class '{0}'.".format([charclass]))
 	
 	if not class_whitelist.is_empty() and not charclass in class_whitelist:
-		GodotLogger.warn("Whitelist is not empty but class '{0}' isn't in it.".format([charclass]))
+		GodotLogger.warn("Whitelist is enabled (not empty) but class '{0}' isn't in it.".format([charclass]))
 		
 		
 	var charClass: CharacterClassResource = J.charclass_resources[charclass].duplicate()
@@ -99,21 +100,21 @@ func apply_stats():
 		return
 	
 	var statsDict: Dictionary
-	for stat in StatsSynchronizerComponent.StatListWithDefaults:
+	for stat: String in StatsSynchronizerComponent.StatListWithDefaults:
 		statsDict[stat] = stats_component.get(stat)
 		
 		#Apply all multipliers from classes for the given stat
-		for charClass in classes:
+		for charClass: CharacterClassResource in classes:
 			statsDict[stat] *= charClass.get_multiplier(stat)
 			
 		#Apply all bonuses from classes
-		for charClass in classes:
+		for charClass: CharacterClassResource in classes:
 			statsDict[stat] += charClass.get_bonus(stat)
 	
 	
 func get_charclass_classes() -> Array[String]:
 	var output: Array[String] = []
-	for charClass in classes:
+	for charClass: CharacterClassResource in classes:
 		output.append(charClass.class_registered)
 	return output
 	
@@ -131,20 +132,20 @@ func sync_response(data: Dictionary):
 func to_json()->Dictionary:
 	var output: Dictionary = {"classes": [], "blacklist": [], "whitelist": [], "max_classes": max_classes}
 	
-	for charClass in classes:
+	for charClass: CharacterClassResource  in classes:
 		output["classes"].append(charClass.class_registered)
 		
-	for charClass in class_blacklist:
+	for charClass: String in class_blacklist:
 		output["blacklist"].append(charClass)
 
-	for charClass in class_whitelist:
+	for charClass: String in class_whitelist:
 		output["whitelist"].append(charClass)
 
 	return output
 	pass
 
 func from_json(data: Dictionary) -> bool:
-	for key in JSON_KEYS:
+	for key: String in JSON_KEYS:
 		if not key in data:
 			GodotLogger.error('Failed to load classes from data, missing "{0}" key'.format([key]))
 			return false
