006f88723d482d1b344dc2cf21ebb901c29f8f15
NancokPS2 <nancok@tuta.io>
2023-12-12 09:35:27 -0300

    Component improvement

--- scripts/components/progressflagcomponent/ProgressFlagComponent.gd
+++ scripts/components/progressflagcomponent/ProgressFlagComponent.gd
@@ -1,9 +1,17 @@
 extends Node
 class_name ProgressFlagsComponent
 
-var flag_system: ProgressFlagSystem
-
+#var flag_system: ProgressFlagSystem
 var flags_stored: Dictionary
+
+var target_node: Node
+
+func _ready() -> void:
+	target_node = get_parent()
+
+	if target_node.get("component_list") != null:
+		target_node.component_list["progress_flags"] = self
+		
 
 func get_flag(flagName: String) -> bool:
 	if not flags_stored.has(flagName):
@@ -12,5 +20,8 @@ func get_flag(flagName: String) -> bool:
 		
 	return flags_stored[flagName]
 
-func sync_flags():
-	
+func sync_flags(flagsSelected: Array): #Array[String], temporarily removed typecast due to a Godot bug
+	G.sync_rpc.progressflags_sync_flags.rpc_id(1, flagsSelected)
+
+func sync_response(flags: Dictionary):
+	flags_stored = flags
