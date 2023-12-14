ba54916fa5abd8fb91286d2187f20551c5c882f6
NancokPS2 <nancok@tuta.io>
2023-12-14 12:10:24 -0300

    Fixed duplicate connections on Milk Lady's shop trigger

--- scenes/npcs/milklady/Milklady.gd
+++ scenes/npcs/milklady/Milklady.gd
@@ -33,9 +33,11 @@ func interact(player: Player):
 		return
 
 	player.dialogue.sync_invoke(player.peer_id, "MilkLady")
-	player.dialogue.dialogue_system_instance.dialogue_finished.connect(
-		_on_dialogue_finished.bind(player.peer_id)
-	)
+	
+	if not player.dialogue.dialogue_system_instance.dialogue_finished.is_connected(_on_dialogue_finished.bind(player.peer_id)):
+		player.dialogue.dialogue_system_instance.dialogue_finished.connect(
+			_on_dialogue_finished.bind(player.peer_id), CONNECT_ONE_SHOT
+		)
 
 
 func _on_dialogue_finished(peerID: int):
