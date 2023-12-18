d63ad71bf17e881b0c25c6f6b647dda1792b644b
NancokPS2 <nancok@tuta.io>
2023-12-18 18:12:23 -0300

    Formatting

--- scenes/npcs/milklady/Milklady.gd
+++ scenes/npcs/milklady/Milklady.gd
@@ -21,11 +21,12 @@ func _ready():
 
 # Add this code until the problem stated below is fixed
 #func interact(player: Player):
-	#if player.get("peer_id") == null:
-		#GodotLogger.error("player node does not have the peer_id variable")
-		#return
+#if player.get("peer_id") == null:
+#GodotLogger.error("player node does not have the peer_id variable")
+#return
 #
-	#shop.open_shop(player.peer_id)
+#shop.open_shop(player.peer_id)
+
 
 # Disabled this piece of code as it breaks a exported project
 # This issue is used to track the problem: https://github.com/jonathaneeckhout/jdungeon/issues/175
@@ -33,7 +34,7 @@ func interact(player: Player):
 	if player.get("peer_id") == null:
 		GodotLogger.error("player node does not have the peer_id variable")
 		return
-		
+
 	player.dialogue.sync_invoke(player.peer_id, "MilkLady")
 
 	if not player.dialogue.dialogue_system_instance.dialogue_finished.is_connected(
@@ -42,6 +43,7 @@ func interact(player: Player):
 		player.dialogue.dialogue_system_instance.dialogue_finished.connect(
 			_on_dialogue_finished.bind(player.peer_id), CONNECT_ONE_SHOT
 		)
+
 
 func _on_dialogue_finished(peerID: int):
 	shop.open_shop(peerID)
--- scripts/singletons/jdungeon.gd
+++ scripts/singletons/jdungeon.gd
@@ -178,17 +178,16 @@ func register_class_resource(charclass_class: String, class_res_path: String):
 ## There'll be a LOT of dialogue, so loading each manually could not only dramatically increase load times, but it could also make errors more likely.
 func register_dialogue_all(dialogue_folder: String):
 	var allFiles: PackedStringArray = DirAccess.get_files_at(dialogue_folder)
-	print(allFiles)
 	for file: String in allFiles:
 		var actualFile: String = file
 		#If the file is an import (like in exported builds) remove the extension.
 		if actualFile.get_extension() == "import":
 			actualFile = actualFile.get_basename()
-		
+
 		#Ignore non resources, if they happened to be in the folder
 		if actualFile.get_extension() != "tres":
 			continue
-		
+
 		var res: Resource = load(dialogue_folder + actualFile)
 
 		#Ignore non dialogues, if they happened to be in the folder
