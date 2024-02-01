450903d064fc6ffafd2221ece792d4561ff8104f
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-02-01 10:59:03 +0100

    build

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ./tools/deploy/prepare.sh $1
+./tools/deploy/build.sh $1
 ./tools/deploy/copy.sh $1
 ./tools/deploy/start.sh $1
--- scenes/terrain/portal/Portal.gd
+++ scenes/terrain/portal/Portal.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 class_name Portal
 
+@export var map: Map = null
+
 @export var destination_server: String = ""
 @export var destination_portal: String = ""
 
@@ -10,21 +12,21 @@ class_name Portal
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	if G.is_server():
-		portal_area.body_entered.connect(_on_body_entered)
-
-
-func _on_body_entered(body: Node2D):
-	if body.get("entity_type") == null:
-		return
-
-	if body.entity_type != J.ENTITY_TYPE.PLAYER:
-		return
-
-	GodotLogger.info("Player=[%s] entered portal=[%s]" % [body.name, name])
-	S.server_rpc.enter_portal.rpc_id(1, body.username, destination_server, destination_portal)
-
-
-func get_portal_location() -> Vector2:
-	return position + portal_location.position
+#func _ready():
+	#if G.is_server():
+		#portal_area.body_entered.connect(_on_body_entered)
+#
+#
+#func _on_body_entered(body: Node2D):
+	#if body.get("entity_type") == null:
+		#return
+#
+	#if body.entity_type != J.ENTITY_TYPE.PLAYER:
+		#return
+#
+	#GodotLogger.info("Player=[%s] entered portal=[%s]" % [body.name, name])
+	#S.server_rpc.enter_portal.rpc_id(1, body.username, destination_server, destination_portal)
+#
+#
+#func get_portal_location() -> Vector2:
+	#return position + portal_location.position
