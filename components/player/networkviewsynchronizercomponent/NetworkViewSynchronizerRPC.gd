b835ef01e1005a0947ed2213344a2b45b3d24a30
Jonathan Eeckhout <44840503+jonathaneeckhout@users.noreply.github.com>
2024-02-12 23:10:21 +0100

    New flow control part 2 (#249)
    * convert statssynchronizer
    
    * remove old lines of code
    
    * convert networkviewsynchronizer

--- components/player/networkviewsynchronizercomponent/NetworkViewSynchronizerRPC.gd
+++ components/player/networkviewsynchronizercomponent/NetworkViewSynchronizerRPC.gd
@@ -6,20 +6,60 @@ class_name NetworkViewSynchronizerRPC
 # The component name for registration in the multiplayer connection's component list.
 const COMPONENT_NAME = "NetworkViewSynchronizerRPC"
 
+enum TYPE {
+	ADD_PLAYER,
+	REMOVE_PLAYER,
+	ADD_ENEMY,
+	REMOVE_ENEMY,
+	ADD_NPC,
+	REMOVE_NPC,
+	ADD_ITEM,
+	REMOVE_ITEM,
+	SYNC_BODIES_IN_VIEW
+}
+
+@export var message_identifier: int = 0
+
 # Reference to the MultiplayerConnection parent node.
 var _multiplayer_connection: MultiplayerConnection = null
+
+var _network_message_handler: NetworkMessageHandler = null
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_network_message_handler = get_parent()
+
 	# Get the MultiplayerConnection parent node.
-	_multiplayer_connection = get_parent()
+	_multiplayer_connection = get_parent().get_parent()
 
 	# Register the component with the parent MultiplayerConnection.
 	_multiplayer_connection.component_list.register_component(COMPONENT_NAME, self)
 
 	# Wait until the multiplayer connection is initialized.
 	await _multiplayer_connection.init_done
+
+
+func handle_message(peer_id: int, message: Array):
+	match message[0]:
+		TYPE.ADD_PLAYER:
+			_add_player(peer_id, message[1], message[2], message[3], message[4])
+		TYPE.REMOVE_PLAYER:
+			_remove_player(peer_id, message[1], message[2])
+		TYPE.ADD_ENEMY:
+			_add_enemy(peer_id, message[1], message[2], message[3], message[4])
+		TYPE.REMOVE_ENEMY:
+			_remove_enemy(peer_id, message[1], message[2])
+		TYPE.ADD_NPC:
+			_add_npc(peer_id, message[1], message[2], message[3], message[4])
+		TYPE.REMOVE_NPC:
+			_remove_npc(peer_id, message[1], message[2])
+		TYPE.ADD_ITEM:
+			_add_item(peer_id, message[1], message[2], message[3], message[4])
+		TYPE.REMOVE_ITEM:
+			_remove_item(peer_id, message[1], message[2])
+		TYPE.SYNC_BODIES_IN_VIEW:
+			_sync_bodies_in_view(peer_id)
 
 
 func add_player(
@@ -29,47 +69,85 @@ func add_player(
 	target_entity_name: String,
 	pos: Vector2
 ):
-	_add_player.rpc_id(peer_id, entity_name, target_entity_peer_id, target_entity_name, pos)
+	# _add_player.rpc_id(peer_id, entity_name, target_entity_peer_id, target_entity_name, pos)
+
+	_network_message_handler.send_message(
+		peer_id,
+		message_identifier,
+		[TYPE.ADD_PLAYER, entity_name, target_entity_peer_id, target_entity_name, pos]
+	)
 
 
 func remove_player(peer_id: int, entity_name: String, target_entity_name: String):
-	_remove_player.rpc_id(peer_id, entity_name, target_entity_name)
+	# _remove_player.rpc_id(peer_id, entity_name, target_entity_name)
+
+	_network_message_handler.send_message(
+		peer_id, message_identifier, [TYPE.REMOVE_PLAYER, entity_name, target_entity_name]
+	)
 
 
 func add_enemy(
 	peer_id: int, entity_name: String, enemy_name: String, enemy_class: String, pos: Vector2
 ):
-	_add_enemy.rpc_id(peer_id, entity_name, enemy_name, enemy_class, pos)
+	# _add_enemy.rpc_id(peer_id, entity_name, enemy_name, enemy_class, pos)
+
+	_network_message_handler.send_message(
+		peer_id, message_identifier, [TYPE.ADD_ENEMY, entity_name, enemy_name, enemy_class, pos]
+	)
 
 
 func remove_enemy(peer_id: int, entity_name: String, enemy_name: String):
-	_remove_enemy.rpc_id(peer_id, entity_name, enemy_name)
+	# _remove_enemy.rpc_id(peer_id, entity_name, enemy_name)
+
+	_network_message_handler.send_message(
+		peer_id, message_identifier, [TYPE.REMOVE_ENEMY, entity_name, enemy_name]
+	)
 
 
 func add_npc(peer_id: int, entity_name: String, npc_name: String, npc_class: String, pos: Vector2):
-	_add_npc.rpc_id(peer_id, entity_name, npc_name, npc_class, pos)
+	# _add_npc.rpc_id(peer_id, entity_name, npc_name, npc_class, pos)
+
+	_network_message_handler.send_message(
+		peer_id, message_identifier, [TYPE.ADD_NPC, entity_name, npc_name, npc_class, pos]
+	)
 
 
 func remove_npc(peer_id: int, entity_name: String, npc_name: String):
-	_remove_npc.rpc_id(peer_id, entity_name, npc_name)
+	# _remove_npc.rpc_id(peer_id, entity_name, npc_name)
+
+	_network_message_handler.send_message(
+		peer_id, message_identifier, [TYPE.REMOVE_ENEMY, entity_name, npc_name]
+	)
 
 
 func add_item(
 	peer_id: int, entity_name: String, item_name: String, item_class: String, pos: Vector2
 ):
-	_add_item.rpc_id(peer_id, entity_name, item_name, item_class, pos)
+	# _add_item.rpc_id(peer_id, entity_name, item_name, item_class, pos)
+
+	_network_message_handler.send_message(
+		peer_id, message_identifier, [TYPE.ADD_ITEM, entity_name, item_name, item_class, pos]
+	)
 
 
 func remove_item(peer_id: int, entity_name: String, item_name: String):
-	_remove_item.rpc_id(peer_id, entity_name, item_name)
+	# _remove_item.rpc_id(peer_id, entity_name, item_name)
+
+	_network_message_handler.send_message(
+		peer_id, message_identifier, [TYPE.REMOVE_ENEMY, entity_name, item_name]
+	)
 
 
 func sync_bodies_in_view():
-	_sync_bodies_in_view.rpc_id(1)
+	# _sync_bodies_in_view.rpc_id(1)
+
+	_network_message_handler.send_message(1, message_identifier, [TYPE.SYNC_BODIES_IN_VIEW])
 
 
-@rpc("call_remote", "authority", "reliable")
-func _add_player(n: String, i: int, u: String, p: Vector2):
+func _add_player(id: int, n: String, i: int, u: String, p: Vector2):
+	if id != 1:
+		return
+
 	var entity: Node = _multiplayer_connection.map.get_entity_by_name(n)
 
 	if entity == null:
@@ -82,12 +160,10 @@ func _add_player(n: String, i: int, u: String, p: Vector2):
 		entity.component_list["networkview_synchronizer"].add_player(i, u, p)
 
 
-@rpc(
-	"call_remote",
-	"authority",
-	"reliable",
-)
-func _remove_player(n: String, u: String):
+func _remove_player(id: int, n: String, u: String):
+	if id != 1:
+		return
+
 	var entity: Node = _multiplayer_connection.map.get_entity_by_name(n)
 
 	if entity == null:
@@ -100,8 +176,10 @@ func _remove_player(n: String, u: String):
 		entity.component_list["networkview_synchronizer"].remove_player(u)
 
 
-@rpc("call_remote", "authority", "reliable")
-func _add_enemy(n: String, en: String, ec: String, p: Vector2):
+func _add_enemy(id: int, n: String, en: String, ec: String, p: Vector2):
+	if id != 1:
+		return
+
 	var entity: Node = _multiplayer_connection.map.get_entity_by_name(n)
 
 	if entity == null:
@@ -114,8 +192,10 @@ func _add_enemy(n: String, en: String, ec: String, p: Vector2):
 		entity.component_list["networkview_synchronizer"].add_enemy(en, ec, p)
 
 
-@rpc("call_remote", "authority", "reliable")
-func _remove_enemy(n: String, en: String):
+func _remove_enemy(id: int, n: String, en: String):
+	if id != 1:
+		return
+
 	var entity: Node = _multiplayer_connection.map.get_entity_by_name(n)
 
 	if entity == null:
@@ -128,8 +208,10 @@ func _remove_enemy(n: String, en: String):
 		entity.component_list["networkview_synchronizer"].remove_enemy(en)
 
 
-@rpc("call_remote", "authority", "reliable")
-func _add_npc(n: String, nn: String, nc: String, p: Vector2):
+func _add_npc(id: int, n: String, nn: String, nc: String, p: Vector2):
+	if id != 1:
+		return
+
 	var entity: Node = _multiplayer_connection.map.get_entity_by_name(n)
 
 	if entity == null:
@@ -142,8 +224,10 @@ func _add_npc(n: String, nn: String, nc: String, p: Vector2):
 		entity.component_list["networkview_synchronizer"].add_npc(nn, nc, p)
 
 
-@rpc("call_remote", "authority", "reliable")
-func _remove_npc(n: String, nn: String):
+func _remove_npc(id: int, n: String, nn: String):
+	if id != 1:
+		return
+
 	var entity: Node = _multiplayer_connection.map.get_entity_by_name(n)
 
 	if entity == null:
@@ -156,8 +240,10 @@ func _remove_npc(n: String, nn: String):
 		entity.component_list["networkview_synchronizer"].remove_npc(nn)
 
 
-@rpc("call_remote", "authority", "reliable")
-func _add_item(n: String, iu: String, ic: String, p: Vector2):
+func _add_item(id: int, n: String, iu: String, ic: String, p: Vector2):
+	if id != 1:
+		return
+
 	var entity: Node = _multiplayer_connection.map.get_entity_by_name(n)
 
 	if entity == null:
@@ -170,8 +256,10 @@ func _add_item(n: String, iu: String, ic: String, p: Vector2):
 		entity.component_list["networkview_synchronizer"].add_item(iu, ic, p)
 
 
-@rpc("call_remote", "authority", "reliable")
-func _remove_item(n: String, iu: String):
+func _remove_item(id: int, n: String, iu: String):
+	if id != 1:
+		return
+
 	var entity: Node = _multiplayer_connection.map.get_entity_by_name(n)
 
 	if entity == null:
@@ -184,11 +272,8 @@ func _remove_item(n: String, iu: String):
 		entity.component_list["networkview_synchronizer"].remove_item(iu)
 
 
-@rpc("call_remote", "any_peer", "reliable")
-func _sync_bodies_in_view():
+func _sync_bodies_in_view(id: int):
 	assert(_multiplayer_connection.is_server(), "This call can only run on the server")
-
-	var id = _multiplayer_connection.multiplayer_api.get_remote_sender_id()
 
 	var user: MultiplayerConnection.User = _multiplayer_connection.get_user_by_id(id)
 	if user == null:
