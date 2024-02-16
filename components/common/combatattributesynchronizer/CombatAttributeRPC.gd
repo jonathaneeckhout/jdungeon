e7e6a6b9b9dcdd2c2792472641064c35976dbfe5
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-02-16 20:36:31 +0100

    add combatattributesynchronizer

---
+++ components/common/combatattributesynchronizer/CombatAttributeRPC.gd
@@ -0,0 +1,80 @@
+extends Node
+
+class_name CombatAttributeSynchronizerRPC
+
+const COMPONENT_NAME = "CombatAttributeSynchronizerRPC"
+
+enum TYPE { SYNC, SYNC_RESPONSE }
+
+@export var message_identifier: int = 0
+
+# Reference to the MultiplayerConnection parent node.
+var _multiplayer_connection: MultiplayerConnection = null
+
+var _network_message_handler: NetworkMessageHandler = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_network_message_handler = get_parent()
+
+	# Get the MultiplayerConnection parent node.
+	_multiplayer_connection = get_parent().get_parent()
+
+	# Register the component with the parent MultiplayerConnection.
+	_multiplayer_connection.component_list.register_component(COMPONENT_NAME, self)
+
+	# Wait until the multiplayer connection is initialized.
+	await _multiplayer_connection.init_done
+
+
+func handle_message(peer_id: int, message: Array):
+	match message[0]:
+		TYPE.SYNC:
+			_request_sync(peer_id, message[1])
+		TYPE.SYNC_RESPONSE:
+			_sync_response(peer_id, message[1], message[2])
+
+
+func request_sync(entity_name: String):
+	_network_message_handler.send_message(1, message_identifier, [TYPE.SYNC, entity_name])
+
+
+#Called by client, runs on server
+func _request_sync(id: int, n: String):
+	assert(_multiplayer_connection.is_server(), "This call can only run on the server")
+
+	# Only allow logged in players
+	if not _multiplayer_connection.is_user_logged_in(id):
+		return
+
+	var entity: Node = _multiplayer_connection.map.get_entity_by_name(n)
+
+	if entity == null:
+		return
+
+	if entity.get("component_list") == null:
+		return
+
+	if entity.component_list.has(CombatAttributeSynchronizerComponent.COMPONENT_NAME):
+		_sync_response.rpc_id(
+			id,
+			n,
+			entity.component_list[CombatAttributeSynchronizerComponent.COMPONENT_NAME].to_json(true)
+		)
+
+
+func _sync_response(id: int, n: String, d: Dictionary):
+	if id != 1:
+		return
+
+	var entity: Node = _multiplayer_connection.map.get_entity_by_name(n)
+
+	if entity == null:
+		return
+
+	if entity.get("component_list") == null:
+		return
+
+	if entity.component_list.has(CombatAttributeSynchronizerComponent.COMPONENT_NAME):
+		entity.component_list[CombatAttributeSynchronizerComponent.COMPONENT_NAME].from_json(d)
---
+++ components/common/combatattributesynchronizer/CombatAttributeSynchronizer.gd
@@ -0,0 +1,120 @@
+extends Node
+
+class_name CombatAttributeSynchronizerComponent
+
+const COMPONENT_NAME: String = "combat_attribute_synchronizer"
+
+@export_group("Config")
+@export var watcher_synchronizer: WatcherSynchronizerComponent
+@export var sync_to_client: bool = false
+
+@export_group("Stats")
+@export var attack_power_min: int = 0
+@export var attack_power_max: int = 5
+@export var attack_speed: float = 0.8
+@export var attack_range: float = 64.0
+@export var defense: int = 0
+@export var movement_speed: float = 300.0
+
+var _target_node: Node
+
+# This value is used to check if the target node is another player
+var _peer_id: int = 0
+
+# Reference to the ClockSynchronizer component for timestamp synchronization.
+var _clock_synchronizer: ClockSynchronizer = null
+
+var _combat_attribute_synchronizer_rpc: CombatAttributeSynchronizerRPC = null
+
+
+func _ready():
+	_target_node = get_parent()
+
+	assert(_target_node.multiplayer_connection != null, "Target's multiplayer connection is null")
+
+	if _target_node.get("component_list") != null:
+		_target_node.component_list[COMPONENT_NAME] = self
+
+	if _target_node.get("peer_id") != null:
+		_peer_id = _target_node.peer_id
+
+	# Get the ClockSynchronizer component.
+	_clock_synchronizer = _target_node.multiplayer_connection.component_list.get_component(
+		ClockSynchronizer.COMPONENT_NAME
+	)
+
+	assert(_clock_synchronizer != null, "Failed to get ClockSynchronizer component")
+
+	# Get the CombatAttributeSynchronizerRPC component.
+	_combat_attribute_synchronizer_rpc = (
+		_target_node
+		. multiplayer_connection
+		. component_list
+		. get_component(CombatAttributeSynchronizerRPC.COMPONENT_NAME)
+	)
+
+	# Ensure the CombatAttributeSynchronizerRPC component is present
+	assert(
+		_combat_attribute_synchronizer_rpc != null,
+		"Failed to get CombatAttributeSynchronizerRPC component"
+	)
+
+	_combat_attribute_synchronizer_rpc.request_sync(_target_node.name)
+
+
+func to_json() -> Dictionary:
+	var output: Dictionary = {
+		"attack_power_min": attack_power_min,
+		"attack_power_max": attack_power_max,
+		"attack_speed": attack_speed,
+		"attack_range": attack_range,
+		"defense": defense,
+		"movement_speed": movement_speed
+	}
+
+	return output
+
+
+func from_json(data: Dictionary) -> bool:
+	if not "attack_power_min" in data:
+		GodotLogger.warn(
+			'Failed to load combat attributes info from data, missing "attack_power_min" key'
+		)
+		return false
+
+	if not "attack_power_max" in data:
+		GodotLogger.warn(
+			'Failed to load combat attributes info from data, missing "attack_power_max" key'
+		)
+		return false
+
+	if not "attack_speed" in data:
+		GodotLogger.warn(
+			'Failed to load combat attributes info from data, missing "attack_speed" key'
+		)
+		return false
+
+	if not "attack_speed" in data:
+		GodotLogger.warn(
+			'Failed to load combat attributes info from data, missing "attack_speed" key'
+		)
+		return false
+
+	if not "defense" in data:
+		GodotLogger.warn('Failed to load combat attributes info from data, missing "defense" key')
+		return false
+
+	if not "movement_speed" in data:
+		GodotLogger.warn(
+			'Failed to load combat attributes info from data, missing "movement_speed" key'
+		)
+		return false
+
+	attack_power_min = data["attack_power_min"]
+	attack_power_max = data["attack_power_max"]
+	attack_speed = data["attack_speed"]
+	attack_range = data["attack_range"]
+	defense = data["defense"]
+	movement_speed = data["movement_speed"]
+
+	return true
