5e97cc90d3a77ff36e2091d4ed150670436dcdfe
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-02-23 21:23:55 +0100

    add experience component

--- scenes/player/statdisplay/StatDisplay.gd
+++ scenes/player/statdisplay/StatDisplay.gd
@@ -6,6 +6,7 @@ class_name StatsDisplay
 @export var health: HealthSynchronizerComponent = null
 @export var energy: EnergySynchronizerComponent = null
 @export var combat: CombatAttributeSynchronizerComponent = null
+@export var experience: ExperienceSynchronizerComponent = null
 
 @export var accept_input: bool = true
 
@@ -29,6 +30,8 @@ var attack_power_max_value_label: Label = $ScrollContainer/StatList/AttackPowerM
 func _ready():
 	health.changed.connect(_on_health_changed)
 	energy.changed.connect(_on_energy_changed)
+	combat.changed.connect(_on_combact_changed)
+	experience.changed.connect(_on_experience_changed)
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -42,9 +45,9 @@ func _renew_values():
 	energy_value_label.text = str(energy.energy)
 	energy_max_value_label.text = str(energy.energy_max)
 	energy_regeneration_value_label.text = str(energy.energy_regen)
-	# level_value_label.text = str(stats.level)
-	# experience_value_label.text = str(stats.experience)
-	# experience_needed_value_label.text = str(stats.experience_needed)
+	level_value_label.text = str(experience.level)
+	experience_value_label.text = str(experience.experience)
+	experience_needed_value_label.text = str(experience.experience_needed)
 	attack_power_min_value_label.text = str(combat.attack_power_min)
 	attack_power_max_value_label.text = str(combat.attack_power_max)
 	defense_value_label.text = str(combat.defense)
@@ -55,4 +58,12 @@ func _on_health_changed():
 
 
 func _on_energy_changed():
+	_renew_values()
+
+
+func _on_combact_changed():
+	_renew_values()
+
+
+func _on_experience_changed():
 	_renew_values()
